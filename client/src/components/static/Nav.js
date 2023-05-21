c54126d8f903fe265de0bcaab2f9ef479c14560d
Anthony Infortunio <contact@anthonyinfortun.io>
2023-05-21 14:28:03 -0400

    add login and logout to nav bar

--- client/src/components/static/Nav.js
+++ client/src/components/static/Nav.js
@@ -25,6 +25,50 @@ export default function Nav(){
       }
     })
   };
+  let getNavMenu=function(){
+    if (!localStorage.getItem('jwt')){
+      return(
+        <div>
+          <div 
+            onClick={()=>{
+              const login = document.querySelector('#login');
+              if (login.classList.contains('hidden')){
+                login.classList.remove('hidden');
+              }else{
+                login.classList.add('hidden');
+              }
+              }}
+            className='nav-dropdown'
+          >
+            <p>Log-In</p>
+            <img alt='drop down menu arrow facing down' src={menuIMG} />
+          </div>
+          {/*leaving method and action here for readability however the submission is handled through handleLogin function*/}
+          <form id='login' className='hidden' method='POST' action='http://localhost:5000/api/user/login'> 
+            <div>
+              <label htmlFor='email'>Email:</label>
+              <input type='email' id='email' name='email' value={emailInput} onChange={(e)=>{setEmailInput(e.target.value)}} />
+            </div>
+            <div>
+              <label htmlFor='password'>Password:</label>
+              <input type='password' id='password' name='password' value={passInput} onChange={(e)=>{setPassInput(e.target.value)}}/>
+            </div>
+            <button onClick={()=>handleLogin()} type='button'>Log In</button>
+          </form>
+        </div>
+      )
+    }else{
+      return(
+        <div>
+          <p onClick={()=>{
+            localStorage.removeItem('jwt');
+            window.location.href='/';
+          }}>Log-Out</p>
+        </div>
+      )
+    }
+    
+  }
   return(
     <div className='nav'>
       <h3>AppZone</h3>
@@ -33,32 +77,9 @@ export default function Nav(){
         <input value={searchInput} onChange={(e)=>{setSearchInput(e.target.value)}} />
         <button>Search</button>
       </form>
-      <div 
-        onClick={()=>{
-          const login = document.querySelector('#login');
-          if (login.classList.contains('hidden')){
-            login.classList.remove('hidden');
-          }else{
-            login.classList.add('hidden');
-          }
-          }}
-        className='nav-dropdown'
-      >
-        <p>Log-In</p>
-        <img alt='drop down menu arrow facing down' src={menuIMG} />
-      </div>
-      {/*leaving method and action here for readability however the submission is handled through handleLogin function*/}
-      <form id='login' className='hidden' method='POST' action='http://localhost:5000/api/user/login'> 
-        <div>
-          <label htmlFor='email'>Email:</label>
-          <input type='email' id='email' name='email' value={emailInput} onChange={(e)=>{setEmailInput(e.target.value)}} />
-        </div>
-        <div>
-          <label htmlFor='password'>Password:</label>
-          <input type='password' id='password' name='password' value={passInput} onChange={(e)=>{setPassInput(e.target.value)}}/>
-        </div>
-        <button onClick={()=>handleLogin()} type='button'>Log In</button>
-      </form>
+      {
+        getNavMenu()
+      }
     </div>
   )
 }
